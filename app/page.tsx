68b02f2f289a80e187d9c367d11fc75ef935712d
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-06-01 13:46:12 +0100

    Add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   const [query, setQuery] = useState("");
@@ -35,59 +36,62 @@ export default function Home() {
     }
   }
   return (
-    <div className="fd-container">
-      <div className="fd-row">
-        <div className="fd-col">
-          <div className="docs-layout-grid-bg docs-layout-grid-bg--color-1">
-            <section
-              className="fd-section"
-              style={{ display: "flex", justifyContent: "center" }}
-            >
-              {/* <input
+    <>
+      <div className="fd-container">
+        <div className="fd-row">
+          <div className="fd-col">
+            <div className="docs-layout-grid-bg docs-layout-grid-bg--color-1">
+              <section
+                className="fd-section"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                {/* <input
                 className="text-black px-2 py-1"
                 onChange={(e) => setQuery(e.target.value)}
               /> */}
 
-              <div
-                className="fd-container fd-form-layout-grid-container fd-form-layout-grid-container--vertical fd-form-group"
-                style={{ maxWidth: "600px" }}
-              >
-                <div className="fd-row fd-form-item">
-                  <div className="fd-col">
-                    <label className="fd-form-label" htmlFor="textarea-1">
-                      Ask the assistant:
-                    </label>
+                <div
+                  className="fd-container fd-form-layout-grid-container fd-form-layout-grid-container--vertical fd-form-group"
+                  style={{ maxWidth: "600px" }}
+                >
+                  <div className="fd-row fd-form-item">
+                    <div className="fd-col">
+                      <label className="fd-form-label" htmlFor="textarea-1">
+                        Ask the assistant:
+                      </label>
+                    </div>
+                    <div className="fd-col">
+                      <textarea
+                        className="fd-textarea"
+                        id="textarea-1"
+                        placeholder="Type your question here. Try to be descriptive and provide the important details."
+                        onChange={(e) => setQuery(e.target.value)}
+                      ></textarea>{" "}
+                    </div>
+                    <button
+                      className="fd-button fd-button--emphasized"
+                      onClick={sendQuery}
+                      disabled={loading}
+                    >
+                      Submit
+                    </button>
                   </div>
-                  <div className="fd-col">
-                    <textarea
-                      className="fd-textarea"
-                      id="textarea-1"
-                      placeholder="Type your question here. Try to be descriptive and provide the important details."
-                      onChange={(e) => setQuery(e.target.value)}
-                    ></textarea>{" "}
-                  </div>
-                  <button
-                    className="fd-button fd-button--emphasized"
-                    onClick={sendQuery}
-                    disabled={loading}
-                  >
-                    Submit
-                  </button>
                 </div>
-              </div>
-            </section>
-            <section
-              className="fd-section"
-              style={{ display: "flex", justifyContent: "center" }}
-            >
-              {loading && <p>Please wait...</p>}
-              {result && <p style={{ maxWidth: "600px" }}>{result}</p>}
-              {/* consider removing this button from the UI once the embeddings are created ... */}
-              {/* <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button> */}
-            </section>
+              </section>
+              <section
+                className="fd-section"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                {loading && <p>Please wait...</p>}
+                {result && <p style={{ maxWidth: "600px" }}>{result}</p>}
+                {/* consider removing this button from the UI once the embeddings are created ... */}
+                {/* <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button> */}
+              </section>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Analytics />
+    </>
   );
 }
